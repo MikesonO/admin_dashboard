4aa087442baacccf86ee8272cd7c5fa104a58d5e
mikeson <mikeson.jnr@gmail.com>
2022-09-18 22:39:44 +0100

    Add on and off toggle to star icon

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 let logoBtn = document.querySelector(".nav-logo");
 let sidebar = document.querySelector(".sidebar");
+let starOff = document.querySelector(".star-off");
+let starOn = document.querySelector(".star-on");
 
 
 logoBtn.onclick = () => {
   sidebar.classList.toggle("active");
+}
+
+starOff.onclick = () => {
+  starOff.classList.toggle("active");
+  if (starOff.classList.contains("active")){
+    starOn.classList.add("active");
+  } else {
+    starOn.classList.remove("active");
+  }
 }
