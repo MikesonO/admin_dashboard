1c2be35ac275cefaba66ee5834021216af2d174e
mikeson <mikeson.jnr@gmail.com>
2022-09-19 00:44:46 +0100

    Fix nav-link hover effects

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ let logoBtn = document.querySelector(".nav-logo");
 let sidebar = document.querySelector(".sidebar");
 let starOff = document.querySelector(".star-off");
 let starOn = document.querySelector(".star-on");
+let navHighlight = document.querySelectorAll(".nav-link");
+
 
 
 logoBtn.onclick = () => {
   sidebar.classList.toggle("active");
+  if (sidebar.classList.contains("active")){
+    for (var i = 0; i < navHighlight.length; i++) {
+      navHighlight[i].classList.remove("highlight");
+    }
+} else{
+  for (var i = 0; i < navHighlight.length; i++) {
+    navHighlight[i].classList.add("highlight");
+  }
+}
 }
 
 starOff.onclick = () => {
